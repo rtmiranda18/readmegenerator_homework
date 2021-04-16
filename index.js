e62b1b48748bab38f5fd69437d4c4e7c5020e655
Reesh Miranda <rtmiranda18@gmail.com>
2021-04-16 13:04:30 +0800

    input badge description and source

--- index.js
+++ index.js
@@ -18,12 +18,20 @@ inquirer
   .then(response => {
     if(response.license == 'MIT'){
         response.badge = '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)';
+        response.badgeDescription = 'A short and simple permissive license with conditions only requiring preservation of copyright and license notices. Licensed works, modifications, and larger works may be distributed under different terms and without source code.';
+        response.badgeDescriptionSource = 'https://choosealicense.com/licenses/mit/';
     }else if(response.license == 'APACHE 2.0'){
         response.badge = '[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)';
+        response.badgeDescription = 'A permissive license whose main conditions require preservation of copyright and license notices. Contributors provide an express grant of patent rights. Licensed works, modifications, and larger works may be distributed under different terms and without source code.';
+        response.badgeDescriptionSource = 'https://choosealicense.com/licenses/apache-2.0/';
     }else if(response.license == 'GPL 3.0'){
         response.badge = '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)';
+        response.badgeDescription = 'Permissions of this strong copyleft license are conditioned on making available complete source code of licensed works and modifications, which include larger works using a licensed work, under the same license. Copyright and license notices must be preserved. Contributors provide an express grant of patent rights.';
+        response.badgeDescriptionSource = 'https://choosealicense.com/licenses/gpl-3.0/';
     }else if(response.license == 'BSD 3'){
         response.badge = '[![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)';
+        response.badgeDescription = 'A simple license that merely requires that all code retain the BSD license notice if redistributed in source code format, or reproduce the notice if redistributed in binary format. It (unlike some other licenses e.g. GPL) does not require that source code be distributed at all.';
+        response.badgeDescriptionSource = 'https://en.wikipedia.org/wiki/BSD_licenses';
     }
     writeUp(response);
   });
@@ -64,7 +72,9 @@ ${data.contributing}
 
 <a name="license"></a>
 ## License
-${data.license}
+${data.license}<br />
+* ${data.badgeDescription}
+* Source: ${data.badgeDescriptionSource}
 
 <a name="test"></a>
 ## Tests
--- results.md
+++ results.md
@@ -3,7 +3,7 @@
 <a name="project_name"></a>
 ## **t**
 
-[![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)
+[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)
 
 ## Table of Contents
 1. [Description](#description)
@@ -32,7 +32,9 @@ t
 
 <a name="license"></a>
 ## License
-BSD 3
+GPL 3.0<br />
+* Permissions of this strong copyleft license are conditioned on making available complete source code of licensed works and modifications, which include larger works using a licensed work, under the same license. Copyright and license notices must be preserved. Contributors provide an express grant of patent rights.
+* Source: https://choosealicense.com/licenses/gpl-3.0/
 
 <a name="test"></a>
 ## Tests
